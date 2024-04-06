d9346de40df41fe14cb69fc9cbbbbaab65cc6932
Wouter Vernaillen <wouter@vernaillen.com>
2024-04-06 16:53:00 +0200

    generated with Nuxt UI Pro - SaaS template

---
+++ nuxt.config.ts
@@ -0,0 +1,31 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  extends: [process.env.NUXT_UI_PRO_PATH || '@nuxt/ui-pro'],
+  modules: [
+    '@nuxt/content',
+    '@nuxt/image',
+    '@nuxt/ui',
+    '@nuxt/fonts',
+    '@nuxthq/studio',
+    '@vueuse/nuxt',
+    'nuxt-og-image',
+  ],
+  hooks: {
+    // Define `@nuxt/ui` components as global to use them in `.md` (feel free to add those you need)
+    'components:extend': (components) => {
+      const globals = components.filter((c) => ['UButton'].includes(c.pascalName))
+
+      globals.forEach((c) => c.global = true)
+    }
+  },
+  ui: {
+    icons: ['heroicons', 'simple-icons']
+  },
+  routeRules: {
+    '/api/search.json': { prerender: true },
+    '/docs': { redirect: '/docs/getting-started', prerender: false }
+  },
+  devtools: {
+    enabled: true
+  }
+})
