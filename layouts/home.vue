dcd635ea7656912f4e7ccd7376d0ef7a0b10119c
Wouter Vernaillen <wouter@vernaillen.com>
2024-04-23 15:49:22 +0200

    svg-left responsiveness

--- layouts/home.vue
+++ layouts/home.vue
@@ -40,7 +40,13 @@ onMounted(() => {
     <div class="svg-right absolute top-0 right-0 left-1/2 lg:left-2/3 pl-8 sm:pl-14 z-[-1] overflow-hidden">
       <SvgoHomeRight class="w-full h-screen" />
     </div>
-    <div class="svg-left absolute bottom-24 left-10 z-[-1] opacity-30 block overflow-hidden">
+    <div
+      class="svg-left absolute
+        bottom-80 -left-10 scale-75
+        sm:bottom-64 sm:left-0 sm:scale-90
+        md:bottom-40 md:left-10 md:scale-100
+        z-[-1] opacity-30 block overflow-hidden"
+    >
       <SvgoHomeLeft class="w-full" />
     </div>
     <ClientOnly>
