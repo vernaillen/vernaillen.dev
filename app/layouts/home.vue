70cbf407c1a92f52574b7a88a2bd8354f5f080d2
Wouter Vernaillen <wouter@vernaillen.com>
2025-07-19 10:20:26 +0200

    remoge blur, keep opacity 20%

--- app/layouts/home.vue
+++ app/layouts/home.vue
@@ -7,7 +7,7 @@ const pageHooks = usePageHooks()
     <div id="top" />
     <UContainer
       class="transition-all duration-300"
-      :class="pageHooks.blurPageContent.value ? 'opacity-20 blur-xs' : 'opacity-100 blur-none'"
+      :class="pageHooks.blurPageContent.value ? 'opacity-20' : 'opacity-100'"
     >
       <slot />
     </UContainer>
